5b057b15a6037bc38952560524a2798775da29c8
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2021-11-16 01:59:45 +0300

    cms

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,99 @@
+import { Heading, majorScale, Pane, Spinner } from 'evergreen-ui'
+import fs from 'fs'
+import matter from 'gray-matter'
+import hydrate from 'next-mdx-remote/hydrate'
+import renderTostring from 'next-mdx-remote/render-to-string'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import path from 'path'
+import React, { FC } from 'react'
+import Container from '../../components/container'
+import HomeNav from '../../components/homeNav'
+import { posts } from '../../content'
+import { Post } from '../../types'
+
+const BlogPost: FC<Post> = ({ source, frontMatter }) => {
+  const content = hydrate(source)
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return (
+      <Pane width="100%" height="100%">
+        <Spinner size={48} />
+      </Pane>
+    )
+  }
+  return (
+    <Pane>
+      <Head>
+        <title>{`Known Blog | ${frontMatter.title}`}</title>
+        <meta name="description" content={frontMatter.summary} />
+      </Head>
+      <header>
+        <HomeNav />
+      </header>
+      <main>
+        <Container>
+          <Heading fontSize="clamp(2rem, 8vw, 6rem)" lineHeight="clamp(2rem, 8vw, 6rem)" marginY={majorScale(3)}>
+            {frontMatter.title}
+          </Heading>
+          <Pane>{content}</Pane>
+        </Container>
+      </main>
+    </Pane>
+  )
+}
+
+BlogPost.defaultProps = {
+  source: '',
+  frontMatter: { title: 'default title', summary: 'summary', publishedOn: '' },
+}
+
+/**
+ * Need to get the paths here
+ * then the the correct post for the matching path
+ * Posts can come from the fs or our CMS
+ */
+export function getStaticPaths() {
+  const postsPath = path.join(process.cwd(), 'posts')
+  const filenames = fs.readdirSync(postsPath)
+  const slugs = filenames.map((name) => {
+    const filePath = path.join(postsPath, name)
+    const file = fs.readFileSync(filePath, 'utf-8')
+    const { data } = matter(file)
+    return data
+  })
+
+  return {
+    paths: slugs.map((s) => ({ params: { slug: s.slug } })),
+    fallback: true, // false, 'blocking'
+  }
+}
+
+export async function getStaticProps({ params }) {
+  let post
+  try {
+    const postsPath = path.join(process.cwd(), 'posts', `${params.slug}.mdx`)
+    post = fs.readFileSync(postsPath, 'utf-8')
+  } catch (e) {
+    const cmsPosts = posts.published.map((p) => {
+      const { data } = matter(p)
+      return data
+    })
+
+    const match = cmsPosts.find((p) => p.slug === params.slug)
+    post = match.content
+  }
+
+  const { data } = matter(post)
+  const mdxSource = await renderTostring(post, { scope: data })
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+    },
+  }
+}
+
+export default BlogPost
---
+++ pages/blog/index.tsx
@@ -0,0 +1,64 @@
+import { majorScale, Pane } from 'evergreen-ui'
+import fs from 'fs'
+import matter from 'gray-matter'
+import path from 'path'
+import React from 'react'
+import Container from '../../components/container'
+import HomeNav from '../../components/homeNav'
+import PostPreview from '../../components/postPreview'
+import { posts as postsFromCMS } from '../../content'
+
+const Blog = ({ posts }) => {
+  return (
+    <Pane>
+      <header>
+        <HomeNav />
+      </header>
+      <main>
+        <Container>
+          {posts.map((post) => (
+            <Pane key={post.title} marginY={majorScale(5)}>
+              <PostPreview post={post} />
+            </Pane>
+          ))}
+        </Container>
+      </main>
+    </Pane>
+  )
+}
+
+Blog.defaultProps = {
+  posts: [],
+}
+export function getStaticProps() {
+  // toml to json
+  const cmsPosts = postsFromCMS.published.map((post) => {
+    const { data } = matter(post)
+    return data
+  })
+
+  // fs
+  const postsPath = path.join(process.cwd(), 'posts')
+  const filenames = fs.readdirSync(postsPath)
+  const filePosts = filenames.map((name) => {
+    const fullPath = path.join(process.cwd(), 'posts', name)
+    const file = fs.readFileSync(fullPath, 'utf-8')
+    const { data } = matter(file)
+    return data
+  })
+
+  const posts = [...cmsPosts, ...filePosts]
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
+}
+
+export default Blog
+
+/**
+ * Need to get the posts from the
+ * fs and our CMS
+ */
